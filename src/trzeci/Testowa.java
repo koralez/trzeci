69e861f0faf35550c909f4fd4a6f95e01d5cce5f
Koralez <Koralez@192.168.1.102>
2017-02-04 11:38:58 +0100

    Dodanie usera

--- src/trzeci/Testowa.java
+++ src/trzeci/Testowa.java
@@ -4,6 +4,14 @@ public class Testowa {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome!");
+		
+		User user = new User();
+		user.setName("Tomasz");
+		user.setAge(19);
+		user.setDescription("blah blah blah");
+		
+		System.out.println(user.getName() +" is "+ user.getAge() + " old" );
+		
 
 	}
 
